2cc9bd86ac93f50b8974d2d77df4af37012d0fde
KSK_Nico <nifrabe@outlook.de>
2019-10-28 18:10:45 +0100

    Ship now rotates on click

--- core/src/com/nico/hotwater/Ship.java
+++ core/src/com/nico/hotwater/Ship.java
@@ -7,13 +7,15 @@ import com.badlogic.gdx.math.Vector2;
 public class Ship extends Sprite {
     public Vector2 position;
     public Vector2 facing;
-    public float speed = 5;
+    public Vector2 targetPosition;
+    public Vector2 targetVector;
+    public float speed = 3;
     public int team;
 
     public Ship(Vector2 position, Vector2 facing) {
         super();
 
-        this.facing = facing;
+        this.facing = facing.nor();
         this.position = position;
     }
 
@@ -21,9 +23,24 @@ public class Ship extends Sprite {
     public void draw(Batch batch) {
         this.setRotation(facing.angle());
         this.setPosition(position.x, position.y);
-
-        position = new Vector2(facing.x * speed + position.x, facing.y * speed + position.y);
-
+        this.update();
         super.draw(batch);
+    }
+
+    public void update() {
+        this.moveToTarget();
+        position = new Vector2(facing.x * speed + position.x, facing.y * speed + position.y);
+        speed -= 0.01;
+        if (speed < 0) {
+            speed = 0;
+        }
+    }
+    public void moveToTarget() {
+        if (targetPosition != null) {
+            this.targetVector = new Vector2( targetPosition.x - position.x, targetPosition.y - position.y );
+            if (targetVector.angle()-facing.angle() > 2) {
+                facing.rotate(0.5f);
+            }
+        }
     }
 }
--- core/src/com/nico/hotwater/screens/GameScreen.java
+++ core/src/com/nico/hotwater/screens/GameScreen.java
@@ -6,17 +6,15 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.nico.hotwater.HotWaterGame;
 import com.nico.hotwater.Resources;
 import com.nico.hotwater.Ship;
 import com.nico.hotwater.ships.Submarine;
+import com.badlogic.gdx.InputProcessor;
 
 
 public class GameScreen implements Screen {
@@ -25,18 +23,60 @@ public class GameScreen implements Screen {
     private Game game;
     public Batch gameBatch;
     public Ship player;
+    public InputProcessor input;
 
     public GameScreen(Game aGame) {
         game = aGame;
         stage = new Stage(new ScreenViewport());
+        input = new InputProcessor() {
+            @Override
+            public boolean keyDown(int keycode) {
+                return false;
+            }
 
+            @Override
+            public boolean keyUp(int keycode) {
+                return false;
+            }
+
+            @Override
+            public boolean keyTyped(char character) {
+                return false;
+            }
+
+            @Override
+            public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+                player.targetPosition = new Vector2(screenX, Gdx.graphics.getHeight() - screenY);
+                return false;
+            }
+
+            @Override
+            public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+                return false;
+            }
+
+            @Override
+            public boolean touchDragged(int screenX, int screenY, int pointer) {
+                return false;
+            }
+
+            @Override
+            public boolean mouseMoved(int screenX, int screenY) {
+                return false;
+            }
+
+            @Override
+            public boolean scrolled(int amount) {
+                return false;
+            }
+        };
+        player = new Submarine(new Vector2(1 , 1), new Vector2(5, 5));
         gameBatch = new SpriteBatch();
-        player = new Submarine(new Vector2(5 , 5), new Vector2(5, 5));
     }
 
     @Override
     public void show() {
-        Gdx.input.setInputProcessor(stage);
+        Gdx.input.setInputProcessor(input);
     }
 
     @Override
