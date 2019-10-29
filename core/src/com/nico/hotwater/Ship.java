5329cb5a56138c65a21dc52c3affe8584e1093bd
KSK_Nico <nifrabe@outlook.de>
2019-10-29 08:21:15 +0100

    Rotation fixed and ships moves now

--- core/src/com/nico/hotwater/Ship.java
+++ core/src/com/nico/hotwater/Ship.java
@@ -3,13 +3,15 @@ package com.nico.hotwater;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
+import java.lang.Math;
 
 public class Ship extends Sprite {
     public Vector2 position;
     public Vector2 facing;
     public Vector2 targetPosition;
     public Vector2 targetVector;
-    public float speed = 3;
+    public float speed = 1;
+    public float turningRate = 1f;
     public int team;
 
     public Ship(Vector2 position, Vector2 facing) {
@@ -28,19 +30,37 @@ public class Ship extends Sprite {
     }
 
     public void update() {
-        this.moveToTarget();
-        position = new Vector2(facing.x * speed + position.x, facing.y * speed + position.y);
-        speed -= 0.01;
+        if (targetPosition != null) {
+            this.moveToTarget();
+            if(this.reachedTarget()) {
+                targetPosition = null;
+            }
+        } else {
+            speed -= 0.1;
+        }
+
         if (speed < 0) {
             speed = 0;
+        } else if (speed > 5) {
+            speed = 5;
         }
+        position = new Vector2(facing.x * speed + position.x, facing.y * speed + position.y);
+
     }
+
     public void moveToTarget() {
-        if (targetPosition != null) {
-            this.targetVector = new Vector2( targetPosition.x - position.x, targetPosition.y - position.y );
-            if (targetVector.angle()-facing.angle() > 2) {
-                facing.rotate(0.5f);
-            }
+        this.targetVector = new Vector2( targetPosition.x - position.x, targetPosition.y - position.y );
+        if (Math.abs(targetVector.angle()-facing.angle()) > 2) {
+            facing.rotate(Math.signum(targetVector.dot((facing.cpy()).rotate90(1))) * turningRate);
+        }
+        speed += 0.01;
+    }
+
+    public boolean reachedTarget() {
+        if (Vector2.dst(position.x, position.y, targetPosition.x, targetPosition.y) < 10) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
